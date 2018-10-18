77d04ccf1b38fe2ec2468fb4e4f401cb393fe8e6
Anusha <anushanadigm@gmail.com>
2018-10-18 23:37:37 +0530

    understanding this determination

--- this.js
+++ this.js
@@ -41,3 +41,28 @@ foo2 = function() {
 };
 
 foo2();
+
+function foo3(baz, bam) {
+  console.log(this.bar, baz, bam);
+}
+
+//hard bound function
+foo3 = foo3.bind(obj1, "baz");
+foo3("bam");
+
+//Four things that happen when you put a new keyword in front of a function
+//1. It creates a brand new empty object
+//2. Newly created object gets linked to another object
+//3. Newly created object in step 1 gets passed in as this context to function call
+//4. If that function doesn't already return it's own object, the new keyword assumes that you meant to return that object that was
+//   passed in , so it returns this
+
+var some = new foo();
+console.log(some.bar);
+console.log(foo.bar);
+
+//determination
+//1. Is the function called by new?
+//2. Is the function called by call() or apply()
+//3. Is the funcction called on a context object
+//4. Default - global object(except strict mode)
