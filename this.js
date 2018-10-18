e429433fd580794d68776f9fecbb012ceb35678f
Anusha <anushanadigm@gmail.com>
2018-10-18 21:51:45 +0530

    understanding this

--- this.js
+++ this.js
@@ -12,3 +12,32 @@ const person = {
 };
 
 person.getName();
+
+//default binding
+var bar = "bar";
+function foo() {
+  console.log(this.bar);
+}
+foo();
+
+//implicit binding
+var obj1 = {
+  bar: "bar1",
+  foo
+};
+
+obj1.foo();
+
+//explicit binding
+foo.call(obj1);
+
+//loosing explicit binding
+foo1 = foo;
+foo1();
+
+//hard binding
+foo2 = function() {
+  foo.call(obj1);
+};
+
+foo2();
